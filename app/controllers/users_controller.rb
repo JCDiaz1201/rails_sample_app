306843ed230f25aa800b9a3418ebf2a07aeb29cb
JCDiaz1201 <jcdiaz1201@gmail.com>
2020-07-31 16:54:40 -0500

    Implemented basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user
+      reset_session
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
